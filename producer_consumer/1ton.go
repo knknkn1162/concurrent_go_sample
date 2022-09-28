a261315e6e532dfa1e49583a1479023d6b91ab3b
Kenta_Nakajima <knknkn1162@gmail.com>
2022-09-28 09:12:44 +0900

    fix 1ton

--- producer_consumer/1ton.go
+++ producer_consumer/1ton.go
@@ -5,7 +5,9 @@ import (
     "time"
 )
 
+const numJobs = 5
 func consume(id int, jobs <-chan int, results chan<- int) {
+    defer close(results)
     for j := range jobs {
         fmt.Println("consumer", id, "started  job", j)
         time.Sleep(time.Second)
@@ -14,26 +16,27 @@ func consume(id int, jobs <-chan int, results chan<- int) {
     }
 }
 
-func produce(jobs chan int) {
-    defer close(jobs)
-    for j := 1; j <= 10; j++ {
-        jobs <- j
-    }
+func produce() chan int {
+    jobs := make(chan int, numJobs)
+    go func() {
+        defer close(jobs)
+        for j := 1; j <= 10; j++ {
+            jobs <- j
+        }
+    }()
+    return jobs
 }
 func main() {
-    const numJobs = 5
-    jobs := make(chan int, numJobs)
-    results := make(chan int, numJobs)
     // 1
-    go produce(jobs)
+    jobs := produce()
 
+    results := make(chan int, numJobs)
     // n: harder
     for w := 1; w <= 3; w++ {
         go consume(w, jobs, results)
     }
 
-    for a := 1; a <= numJobs; a++ {
-        res := <-results
+    for res := range results {
         fmt.Printf("receive %v\n", res)
     }
 }
