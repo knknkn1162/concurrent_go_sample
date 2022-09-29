8c6dd4b9a128e7327dff8682498e3cf9e5e5f2d3
Kenta_Nakajima <knknkn1162@gmail.com>
2022-09-29 15:09:16 +0900

    producer-consumer 1to1

--- producer_consumer/1to1.go
+++ producer_consumer/1to1.go
@@ -5,31 +5,32 @@ import (
     "time"
 )
 
-func consume(jobs <-chan int, results chan<- int) {
-    defer close(results)
-    for j := range jobs {
-        fmt.Println("consumer", "started  job")
-        time.Sleep(time.Second)
-        fmt.Println("consumer", "finished job")
-        results <- j * 2
-    }
+func consume(jobs <-chan int) <-chan int{
+    done := make(chan int)
+    go func() {
+        defer close(done)
+        for j := range jobs {
+            fmt.Printf("started  job %v", j)
+            time.Sleep(time.Second)
+            fmt.Println(" => finished job")
+        }
+    }()
+    return done
 }
 
-func produce(jobs chan int) {
-    defer close(jobs)
-    for j := 1; j <= 5; j++ {
-        jobs <- j
-    }
-}
-func main() {
+func produce() <-chan int{
     jobs := make(chan int)
-    results := make(chan int)
-    // 1
-    go produce(jobs)
-    // 1
-    go consume(jobs, results)
+    go func() {
+        defer close(jobs)
+        for j := 1; j <= 5; j++ {
+            jobs <- j
+        }
+    }()
+    return jobs
+}
 
-    for n := range results {
-        fmt.Printf("receive %v\n", n)
-    }
+func main() {
+    // 1
+    jobs := produce()
+    <-consume(jobs)
 }
